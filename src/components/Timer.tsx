8f4a481654c426551b59f2271f9b7d1d76ac6da1
huytran17 <alpha.lloyd1368@gmail.com>
2024-03-09 17:59:22 +0700

    test

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -3,21 +3,34 @@ import { useEffect, useState } from "react";
 export default function Timer() {
   const [second, setSecond] = useState(0);
   const [intervalId, setIntervalId] = useState<any>(null);
+  const [isStopped, setIsStopped] = useState(false);
 
   useEffect(() => {
+    stop();
+
     const interval = setInterval(() => setSecond((v) => v + 1), 1000);
 
     setIntervalId(interval);
 
     return () => stop();
-  }, []);
+  }, [isStopped]);
 
-  const stop = () => clearInterval(intervalId);
+  const stop = () => {
+    if (!intervalId) {
+      return;
+    }
+
+    clearInterval(intervalId);
+    setIntervalId(null);
+  };
+
+  const resume = () => !intervalId && setIsStopped((v) => !v);
 
   return (
     <>
       <div>Second: {second}</div>
       <button onClick={stop}>Stop</button>
+      <button onClick={resume}>Resume</button>
     </>
   );
 }
